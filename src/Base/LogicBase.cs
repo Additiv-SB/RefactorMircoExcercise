5caf0f4e9e675b401e2382e6bea3778f347fefb9
Sergejs Truksans <sergejs.truksans@28stone.com>
2020-09-04 12:50:50 +0200

    Infrastrucute changes to ensure Test project can resolve dependencies

---
+++ src/Base/LogicBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TDDMicroExercises.Infrastructure;
+
+namespace TDDMicroExercises.Base
+{
+    /// <summary>
+    /// Purpose of the class to provide ServiceProvider to all the logic(service) classes, to be able to retrieve dependencies without injecting it via ctor
+    /// When external dependencies will stop using default ctor, class can be removed
+    /// Or class may be used to store other common dependencies, like audit/loggin services or etc.
+    /// </summary>
+    public abstract class LogicBase
+    {
+        /// <summary>
+        /// Service Provider
+        /// Currently Test project will be able to access internals
+        /// Ideally dependencies should be injected via constructor
+        /// </summary>
+        internal IServiceProvider ServiceProvider => AppServiceProvider.ServiceProvider;
+    }
+}
