4b79e12f56328fd7086471e8203e3ba6004bb92a
Sergejs Truksans <sergejs.truksans@28stone.com>
2020-09-04 12:46:37 +0200

    Update framework from 4.5.2 to 4.8 Added DI

---
+++ src/Infrastructure/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TDDMicroExercises.Infrastructure
+{
+    public static class AppServiceProvider
+    {
+        /// <summary>
+        /// Registering dependencies and building ServiceProvider to ensure external assemblies won't brake
+        /// In other cases app pipelines could be responsible for that
+        /// </summary>
+        public static readonly IServiceProvider ServiceProvider = Run();
+
+        private static IServiceProvider Run()
+        {
+            //Register dependencies and init static ServiceProvider
+            return new ServiceCollection()
+                .AddAppDependencies()
+                .BuildServiceProvider();
+
+
+        }
+    }
+}
