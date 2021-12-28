890e680bb0bb6d58ea1ec28ff086cca8cf77a9d6
kseniya-kalita <kseniya.kalita@innowise-group.com>
2021-12-28 09:35:42 +0300

    Add unit tests for TirePressureMonitoringSystem/Alarm class.

---
+++ TDDMicroExercises.Tests/TirePressureMonitoringSystem/AlarmTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using TDDMicroExercises.TirePressureMonitoringSystem;
+using Xunit;
+
+namespace TDDMicroExercises.Tests.TirePressureMonitoringSystem
+{
+    public class AlarmTests
+    {
+        [Fact]
+        public void AlarmOn_WhenAlarmObjectCreated_SetToDefault()
+        {
+            bool defaultAlarmOnValue = false;
+            
+            Alarm alarm = new Alarm();
+
+            alarm.AlarmOn.Should().Be(defaultAlarmOnValue);
+        }
+    }
+
+}
+
