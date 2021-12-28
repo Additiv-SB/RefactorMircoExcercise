d4a5126ff1cfd8784e0214b7c5234faee7d09058
kseniya-kalita <kseniya.kalita@innowise-group.com>
2021-12-28 10:51:44 +0300

    Add unit tests for TelemetrySystem/TelemetryDiagnosticControls class.

---
+++ TDDMicroExercises.Tests/TelemetrySystem/TelemetryDiagnosticControlsTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using TDDMicroExercises.TelemetrySystem;
+using Xunit;
+
+namespace TDDMicroExercises.Tests.TelemetrySystem
+{
+    public class TelemetryDiagnosticControlsTests
+    {
+        [Fact]
+        public void DiagnosticInfo_WhenObjectCreated_SetToDefaultValue()
+        {
+            string defaultDiagnosticInfoValue = string.Empty;
+
+            TelemetryDiagnosticControls telemetryDiagnosticControls = new TelemetryDiagnosticControls();
+
+            telemetryDiagnosticControls.DiagnosticInfo.Should().Be(defaultDiagnosticInfoValue);
+        }
+        
+        [Theory]
+        [InlineData("some diagnostic info")]
+        [InlineData("another diagnostic info")]
+        public void DiagnosticInfo_WhenSetValue_SetToThisValue(string expectedDiagnosticInfoValue)
+        {
+            TelemetryDiagnosticControls telemetryDiagnosticControls = new TelemetryDiagnosticControls();
+            telemetryDiagnosticControls.DiagnosticInfo = expectedDiagnosticInfoValue;
+
+            telemetryDiagnosticControls.DiagnosticInfo.Should().Be(expectedDiagnosticInfoValue);
+        }
+    }
+}
