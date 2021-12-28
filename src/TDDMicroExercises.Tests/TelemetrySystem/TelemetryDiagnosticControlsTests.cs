ff37b8ffd22a0bd7200e090cc4da7b26894c65f4
kseniya-kalita <kseniya.kalita@innowise-group.com>
2021-12-28 11:33:17 +0300

    Move Test project inside src.

--- src/TDDMicroExercises.Tests/TelemetrySystem/TelemetryDiagnosticControlsTests.cs
+++ src/TDDMicroExercises.Tests/TelemetrySystem/TelemetryDiagnosticControlsTests.cs
@@ -1,11 +1,15 @@
 ﻿using FluentAssertions;
+using Moq;
+using System;
 using TDDMicroExercises.TelemetrySystem;
+using TDDMicroExercises.TelemetrySystem.Interfaces;
 using Xunit;
 
 namespace TDDMicroExercises.Tests.TelemetrySystem
 {
     public class TelemetryDiagnosticControlsTests
     {
+        #region tests for unrefactored code
         [Fact]
         public void DiagnosticInfo_WhenObjectCreated_SetToDefaultValue()
         {
@@ -26,5 +30,33 @@ namespace TDDMicroExercises.Tests.TelemetrySystem
 
             telemetryDiagnosticControls.DiagnosticInfo.Should().Be(expectedDiagnosticInfoValue);
         }
+
+        #endregion
+
+        #region tests for refactored code
+
+        [Fact]
+        public void CheckTransmission_WhenTelemetryClientConnectionThrowsArgumentNullException_ThrowsArgumentNullException()
+        {
+            var expectedExcMessage = "expected message";
+            var mockedTelemetryClient = new Mock<ITelemetryClient>();
+            mockedTelemetryClient.Setup(x => x.Connect(It.IsAny<string>())).Throws(new ArgumentNullException(expectedExcMessage));
+            
+            TelemetryDiagnosticControls telemetryDiagnosticControls = new TelemetryDiagnosticControls(mockedTelemetryClient.Object);
+            
+            try
+            {
+                telemetryDiagnosticControls.CheckTransmission();
+                Assert.NotNull("Exception should be thrown");
+            }
+            catch(ArgumentNullException ex)
+            {
+                telemetryDiagnosticControls;
+            }
+
+
+        }
+
+        #endregion
     }
 }
