f8670d87dbe679319127c0a2b8be79e93b45f550
Joanna <Joanna@ASIA>
2022-08-15 18:16:58 +0200

    Add unit tests to Alarm class

---
+++ src/TirePressureMonitoringSystem.Tests/AlarmTests.cs
@@ -0,0 +1,47 @@
+﻿using Moq;
+using NUnit.Framework;
+
+namespace TDDMicroExercises.TirePressureMonitoringSystem.Tests
+{
+    public class AlarmTests
+    {
+        [Test]
+        public void Check_PsiPressureValueBelowLowThreshold_AlarmOn_ShouldBeTrue()
+        {
+            var mockSensor = new Mock<ISensor>();
+            mockSensor.Setup(x => x.PopNextPressurePsiValue()).Returns(16);
+
+            var alarm = new Alarm(mockSensor.Object);
+            alarm.Check();
+
+            Assert.IsTrue(alarm.AlarmOn);
+        }
+
+        [Test]
+        public void Check_PsiPressureValueAboveHighThreshold_AlarmOn_ShouldBeTrue()
+        {
+            var mockSensor = new Mock<ISensor>();
+            mockSensor.Setup(x => x.PopNextPressurePsiValue()).Returns(22);
+
+            var alarm = new Alarm(mockSensor.Object);
+            alarm.Check();
+
+            Assert.IsTrue(alarm.AlarmOn);
+        }
+
+        [TestCase(17)]
+        [TestCase(21)]
+        [TestCase(18)]
+        [TestCase(20)]
+        public void Check_PsiPressureValueEqualOrBetweenThresholds_AlarmOn_ShouldBeFalse(double psiPressureValue)
+        {
+            var mockSensor = new Mock<ISensor>();
+            mockSensor.Setup(x => x.PopNextPressurePsiValue()).Returns(psiPressureValue);
+
+            var alarm = new Alarm(mockSensor.Object);
+            alarm.Check();
+
+            Assert.IsFalse(alarm.AlarmOn);
+        }
+    }
+}
