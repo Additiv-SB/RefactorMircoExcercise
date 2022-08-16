8da2b2ff5515114961d3fffd12a8bee24542e415
dinokraljusic <dinokraljusic@hotmail.com>
2022-08-16 20:47:10 +0200

    refactored alarm and added tests

--- RefactorMicroExercise.Tests/TirePressureMonitoringSystem/AlarmTests.cs
+++ RefactorMicroExercise.Tests/TirePressureMonitoringSystem/AlarmTests.cs
@@ -1,23 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TDDMicroExercises.TirePressureMonitoringSystem;
+﻿using TDDMicroExercises.TirePressureMonitoringSystem;
 using Xunit;
+using Moq;
 
 namespace RefactorMicroExercise.Tests.TirePressureMonitoringSystem
 {
     public class AlarmTests
     {
         [Fact]
-        public void AlarmOn_SHouldReturnFalseByDefault()
+        public void AlarmOn_ShouldReturnFalseByDefault()
         {
             bool defaultValue = false;
 
             Alarm alarm = new Alarm();
 
             Assert.Equal(defaultValue, alarm.AlarmOn);
+        }
+
+        [Theory]
+        [InlineData(17, false)]
+        [InlineData(16, true)]
+        [InlineData(22, true)]
+        [InlineData(21.01, true)]
+        [InlineData(17.01, false)]
+        public void Check_WhenSensorPoppedValidValu_ReturnsExpected(double sensorValue, bool expectedResult)
+        {
+            // Arrange
+            Mock<ISensor> mockSensor = new Mock<ISensor>();
+            mockSensor.Setup(x => x.PopNextPressurePsiValue()).Returns(sensorValue);
+
+            Alarm alarm = new Alarm(mockSensor.Object);
+
+            // Act
+            alarm.Check();
+
+            // Assert
+            Assert.Equal(expectedResult, alarm.AlarmOn);
         }
     }
 }
---
+++ src/TirePressureMonitoringSystem/AlarmConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace TDDMicroExercises.TirePressureMonitoringSystem
+{
+    public static class AlarmConfiguration
+    {
+        public static double LowPressureTreshold = 17;
+
+        public static double HighPressureThreshold  = 21;
+    }
+}
