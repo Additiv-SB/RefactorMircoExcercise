285f08e4314cd0a002ca5270632f1d5be23bd884
dinokraljusic <dinokraljusic@hotmail.com>
2022-08-16 19:54:38 +0200

    added a test for Alarm clas before refactor

---
+++ RefactorMicroExercise.Tests/TirePressureMonitoringSystem/AlarmTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDDMicroExercises.TirePressureMonitoringSystem;
+using Xunit;
+
+namespace RefactorMicroExercise.Tests.TirePressureMonitoringSystem
+{
+    public class AlarmTests
+    {
+        [Fact]
+        public void AlarmOn_SHouldReturnFalseByDefault()
+        {
+            bool defaultValue = false;
+
+            Alarm alarm = new Alarm();
+
+            Assert.Equal(defaultValue, alarm.AlarmOn);
+        }
+    }
+}
