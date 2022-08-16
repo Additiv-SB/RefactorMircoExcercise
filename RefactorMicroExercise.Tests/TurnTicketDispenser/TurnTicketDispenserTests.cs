da8d8ce5a4bf8d8c55cf2c778d09e787fcc322cd
dinokraljusic <dinokraljusic@hotmail.com>
2022-08-17 00:28:33 +0200

    added TurnTicket tests

---
+++ RefactorMicroExercise.Tests/TurnTicketDispenser/TurnTicketDispenserTests.cs
@@ -0,0 +1,31 @@
+﻿using TDDMicroExercises.TurnTicketDispenser;
+using Xunit;
+
+namespace RefactorMicroExercise.Tests.TurnTicketDispenser
+{
+    public class TurnTicketDispenserTests
+    {
+        [Fact]
+        public void GetTurnTicket_ReturnsNotNull()
+        {
+            TicketDispenser ticketDispenser = new TicketDispenser();
+
+            TurnTicket turnTicket = ticketDispenser.GetTurnTicket();
+
+            Assert.NotNull(turnTicket);
+        }
+
+        [Fact]
+        public void GetTurnTicket_TurnTicketWithRightNumber()
+        {
+            TicketDispenser ticketDispenser = new TicketDispenser();
+
+            for(int i=0; i < 5; i++)
+            {
+                TurnTicket turnTicket = ticketDispenser.GetTurnTicket();
+                Assert.Equal(i, turnTicket.TurnNumber);
+            }
+        }
+
+    }
+}
