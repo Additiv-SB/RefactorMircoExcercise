9e1ed89167465c4406008922f8c88f8cc857c836
Amir Zec <amir.zec@startify.ba>
2022-09-13 14:34:45 +0200

    Added Constants folder and created static class TirePressureMonitoringSystemConstants for constants

---
+++ src/TirePressureMonitoringSystem/Constants/TirePressureMonitoringSystemConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace TDDMicroExercises.TirePressureMonitoringSystem.Constants
+{
+    public static class TirePressureMonitoringSystemConstants
+    {
+        public static double Offset = 16;
+        public static double LowPressureThreshold = 17;
+        public static double HighPressureThreshold = 21;
+    }
+}
