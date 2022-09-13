34099aa9efdfac49b566fb88846444ffe8627d85
Amir Zec <amir.zec@startify.ba>
2022-09-13 14:31:22 +0200

    Created Abstractions folder with interfaces IAlarm and ISensor.

---
+++ src/TirePressureMonitoringSystem/Abstractions/ISensor.cs
@@ -0,0 +1,10 @@
+﻿namespace TDDMicroExercises.TirePressureMonitoringSystem.Abstractions
+{
+    /// <summary>
+    /// Entry point for handling Tire pressure.
+    /// </summary>
+    public interface ISensor
+    {
+        double PopNextPressurePsiValue();
+    }
+}
