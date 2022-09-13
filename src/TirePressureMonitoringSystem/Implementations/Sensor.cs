8857625ecdc935c5e53616561f04d78e353e8184
Amir Zec <amir.zec@startify.ba>
2022-09-13 14:50:54 +0200

    Change Sensor class to satisfy functionality of project

--- src/TirePressureMonitoringSystem/Implementations/Sensor.cs
+++ src/TirePressureMonitoringSystem/Implementations/Sensor.cs
@@ -1,6 +1,29 @@
-﻿namespace TDDMicroExercises.TirePressureMonitoringSystem.Implementations
+﻿using System;
+using TDDMicroExercises.TirePressureMonitoringSystem.Abstractions;
+using TDDMicroExercises.TirePressureMonitoringSystem.Constants;
+
+namespace TDDMicroExercises.TirePressureMonitoringSystem.Implementations
 {
-    public class Sensor
+    public class Sensor : ISensor
     {
+        //
+        // The reading of the pressure value from the sensor is simulated in this implementation.
+        // Because the focus of the exercise is on the other class.
+        //
+        readonly Random _randomPressureSampleSimulator = new Random();
+
+
+        public double PopNextPressurePsiValue()
+        {
+            double pressureTelemetryValue = ReadPressureSample();
+
+            return TirePressureMonitoringSystemConstants.Offset + pressureTelemetryValue;
+        }
+
+        private double ReadPressureSample()
+        {
+            // Simulate info read from a real sensor in a real tire
+            return 6 * _randomPressureSampleSimulator.NextDouble() * _randomPressureSampleSimulator.NextDouble();
+        }
     }
 }
