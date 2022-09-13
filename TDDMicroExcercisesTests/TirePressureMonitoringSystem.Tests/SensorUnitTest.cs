2b99078d34b9b1a49d5d91c8ba66b8de5dabba22
Amir Zec <amir.zec@startify.ba>
2022-09-13 15:14:32 +0200

    Test methods added to SensorUnitTest class

--- TDDMicroExcercisesTests/TirePressureMonitoringSystem.Tests/SensorUnitTest.cs
+++ TDDMicroExcercisesTests/TirePressureMonitoringSystem.Tests/SensorUnitTest.cs
@@ -1,6 +1,30 @@
-﻿namespace TDDMicroExcercisesTests.TirePressureMonitoringSystem.Tests
+﻿using Moq;
+using TDDMicroExercises.TirePressureMonitoringSystem.Abstractions;
+using Xunit;
+
+namespace TDDMicroExcercisesTests.TirePressureMonitoringSystem.Tests
 {
     public class SensorUnitTest
     {
+
+        [Fact]
+        public void CheckReadPressureSample_ShouldBeTrueOnSameValue()
+        {
+            // Arrange
+            Mock<ISensor> sensorMock = new Mock<ISensor>();
+            sensorMock.Setup(x => x.PopNextPressurePsiValue()).Returns(6);
+
+            Assert.Equal(6, sensorMock.Object.PopNextPressurePsiValue());
+        }
+
+        [Fact]
+        public void PopNextPressurePsiValue_ShouldBeTrueOMocked()
+        {
+            // Arrange
+            Mock<ISensor> sensorMock = new Mock<ISensor>();
+            sensorMock.Setup(s => s.PopNextPressurePsiValue()).Returns(25);
+
+            Assert.Equal(25, sensorMock.Object.PopNextPressurePsiValue());
+        }
     }
 }
