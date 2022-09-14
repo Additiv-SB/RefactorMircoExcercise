68502a0d34700d172f1de16497132f5fcffe53dd
Merjem1123 <mpoplata@gmail.com>
2022-09-14 17:02:17 +0200

    Added project reference

---
+++ TDDMicroExcerciseTests/TelemetrySystemTests.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using TDDMicroExercises.TelemetrySystem.Abstractions;
+using TDDMicroExercises.TelemetrySystem.Implementations;
+
+namespace TDDMicroExcerciseTests
+{
+    public class TelemetrySystemTests
+    {
+        [Fact]
+        public void CheckTransmission_UnableToConnect_ShouldThrowException()
+        {
+            // Arrange
+            string expectedMessage = "Unable to connect.";
+            Mock<ITelemetryClient> mockTelemetryClient = new Mock<ITelemetryClient>();
+            Mock<ITelemetryTransmitter> mockTelemetryTransmitter = new Mock<ITelemetryTransmitter>();
+            mockTelemetryClient.Setup(x => x.Disconnect()).Throws(new Exception(expectedMessage));
+            TelemetryDiagnosticControls telemetryDiagnosticControls = new TelemetryDiagnosticControls(mockTelemetryClient.Object, mockTelemetryTransmitter.Object);
+
+            // Act
+            var exception = Assert.Throws<Exception>(() => telemetryDiagnosticControls.CheckTransmission());
+
+            // Assert
+            Assert.Equal(expectedMessage, exception.Message);
+        }
+    }
+}
