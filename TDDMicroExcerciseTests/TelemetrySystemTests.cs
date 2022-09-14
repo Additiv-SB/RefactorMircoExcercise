a8c7cb6535b17b114cbd2d08ddf2b3c611ec877a
Merjem1123 <mpoplata@gmail.com>
2022-09-14 17:03:37 +0200

    Diagnosticinfonotemptywhenonlineistrue test added

--- TDDMicroExcerciseTests/TelemetrySystemTests.cs
+++ TDDMicroExcerciseTests/TelemetrySystemTests.cs
@@ -1,5 +1,6 @@
 ﻿using Moq;
 using TDDMicroExercises.TelemetrySystem.Abstractions;
+using TDDMicroExercises.TelemetrySystem.Constants;
 using TDDMicroExercises.TelemetrySystem.Implementations;
 
 namespace TDDMicroExcerciseTests
@@ -21,6 +22,27 @@ namespace TDDMicroExcerciseTests
 
             // Assert
             Assert.Equal(expectedMessage, exception.Message);
+        }
+
+        [Fact]
+        public void DiagnosticInfo_NotEmpty_WhenOnlineIsTrue()
+        {
+            // Arrange
+            Mock<ITelemetryClient> mockTelemetryClient = new Mock<ITelemetryClient>();
+            Mock<ITelemetryTransmitter> mockTelemetryTransmitter = new Mock<ITelemetryTransmitter>();
+            mockTelemetryClient.Setup(x => x.Connect(It.IsAny<string>()));
+            mockTelemetryClient.Setup(x => x.OnlineStatus).Returns(true);
+            mockTelemetryTransmitter.Setup(x => x.Send(It.IsAny<string>()));
+            mockTelemetryTransmitter.Setup(x => x.Receive()).Returns(TelemetrySystemConstants.DiagnosticMessage);
+
+            TelemetryDiagnosticControls telemetryDiagnosticControls = new TelemetryDiagnosticControls(mockTelemetryClient.Object, mockTelemetryTransmitter.Object);
+
+            // Act
+            telemetryDiagnosticControls.CheckTransmission();
+
+            // Assert
+            Assert.NotEqual(string.Empty, telemetryDiagnosticControls.DiagnosticInfo);
+            Assert.Equal(telemetryDiagnosticControls.DiagnosticInfo, TelemetrySystemConstants.DiagnosticMessage);
         }
     }
 }
