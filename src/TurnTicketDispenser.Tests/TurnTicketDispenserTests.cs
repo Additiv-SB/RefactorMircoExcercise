658f9e51da16be01522bc8dc41510ed269e59cd0
martyna0604 <msalwa0604@gmail.com>
2022-09-23 15:11:37 +0200

    test: Added tests to TicketDispenser

---
+++ src/TurnTicketDispenser.Tests/TurnTicketDispenserTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using TDDMicroExercises.TurnTicketDispenser.Interfaces;
+
+namespace TDDMicroExercises.TurnTicketDispenser.Tests
+{
+    [TestFixture]
+    public class TurnTicketDispenserTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            _ticketDispenser = new TicketDispenser();
+        }
+
+        private ITicketDispenser _ticketDispenser;
+
+        [Test]
+        public void Should_ReturnTurnTicket_When_GetTurnTicketMethodCalled()
+        {
+            var expectedResult = new TurnTicket(0);
+            var result = _ticketDispenser.GetTurnTicket();
+
+            Assert.That(result.TurnNumber, Is.EqualTo(expectedResult.TurnNumber));
+        }
+
+        [Test]
+        public void Should_ReturnTurnTicketType_When_GetTurnTicketMethodCalled()
+        {
+            var expectedType = typeof(TurnTicket);
+            var result = _ticketDispenser.GetTurnTicket();
+
+            Assert.That(result, Is.TypeOf(expectedType));
+        }
+
+        [Test]
+        public void Should_ReturnTwoTurnTicketsWithDifferentTurnNumbers_When_GetTurnTicketMethodCalled()
+        {
+            var ticket1 = _ticketDispenser.GetTurnTicket();
+            var ticket2 = _ticketDispenser.GetTurnTicket();
+
+            Assert.That(ticket1.TurnNumber, Is.Not.EqualTo(ticket2.TurnNumber));
+        }
+
+
+        [Test]
+        public void Should_ReturnTwoTurnTicketsWithDifferentTurnNumbers_When_GetTurnTicketMethodCalledByTwoTicketDispensers()
+        {
+            var ticketDispenser = new TicketDispenser();
+            var ticket1 = ticketDispenser.GetTurnTicket();
+            var ticket2 = _ticketDispenser.GetTurnTicket();
+
+            Assert.That(ticket1.TurnNumber, Is.Not.EqualTo(ticket2.TurnNumber));
+        }
+    }
+}
