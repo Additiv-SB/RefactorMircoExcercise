109619cb13c535ea9234231d8b8019fe3bc14c08
Bianca-Alexandra Suciu <Bianca.Suciu@theaccessgroup.com>
2022-09-26 16:03:07 +0300

    Adding Test Project

---
+++ TDDMicroExercises.Test.Unit/TirePressureMonitoringSystemTest.cs
@@ -0,0 +1,11 @@
+namespace TDDMicroExercises.Test.Unit
+{
+    [TestClass]
+    public class TirePressureMonitoringSystemTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
