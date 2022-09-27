fa638a569ee4f56844e85dbdf75e2178371e1c6d
Bianca-Alexandra Suciu <Bianca.Suciu@theaccessgroup.com>
2022-09-27 12:17:39 +0300

    Adding initial test for UnicodeFileToHtmlTextConverter - failing

---
+++ TDDMicroExercises.Test.Unit/UnicodeFileToHtmlTextConverterTest.cs
@@ -0,0 +1,20 @@
+﻿namespace TDDMicroExercises.Test.Unit
+{
+    [TestClass]
+    public class UnicodeFileToHtmlTextConverterTest
+    {
+        [TestMethod]
+        public void AddNewLine()
+        {
+            //Arrange
+            string textSource = "First Line<br /> Second Line<br /> Third Line"; 
+            var convert = new UnicodeFileToHtmlTextConverter.UnicodeFileToHtmlTextConverter(textSource);
+
+            //Act
+            string result = convert.ConvertToHtml();
+
+            //Assert
+            Assert.AreEqual(result, textSource);
+        }
+    }
+}
