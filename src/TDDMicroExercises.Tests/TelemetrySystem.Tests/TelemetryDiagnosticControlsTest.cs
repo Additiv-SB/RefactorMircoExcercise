24f5b82ce92e4a8da75f77ee313c550a8c1da8e7
obimelo <okenobi79@gmail.com>
2022-10-01 18:44:24 +0100

    Add Unit Test for class TelemetryDiagnosticControls

---
+++ src/TDDMicroExercises.Tests/TelemetrySystem.Tests/TelemetryDiagnosticControlsTest.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using TDDMicroExercises.TelemetrySystem;
+using Xunit;
+
+namespace TDDMicroExercises.Tests.TelemetrySystem.Tests
+{
+    public sealed class TelemetryDiagnosticControlsTest
+    {
+        [Fact]
+        public void CheckTransmission_should_send_a_diagnostic_message_and_receive_a_status_message_response()
+        {
+            // Arrange
+            TelemetryDiagnosticControls tdc = new();
+
+            // Act
+            Action act = () => tdc.CheckTransmission();
+
+            // Assert
+            act.Should().NotThrow();
+        }
+    }
+}
