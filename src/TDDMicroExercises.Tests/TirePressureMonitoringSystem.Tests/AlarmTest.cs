0dafa4eadf76c2a589ef0286918445ef57fadc0f
obimelo <okenobi79@gmail.com>
2022-10-01 22:00:24 +0100

    Add Unit Test for class Alarm

---
+++ src/TDDMicroExercises.Tests/TirePressureMonitoringSystem.Tests/AlarmTest.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using TDDMicroExercises.TirePressureMonitoringSystem;
+using Xunit;
+
+namespace TDDMicroExercises.Tests.TirePressureMonitoringSystem.Tests
+{
+    public sealed class AlarmTest
+    {
+        [Fact]
+        public void Check_should_verify_pressure_value_and_set_alarme_on()
+        {
+            // Arrange
+            Alarm alarme = new();
+
+            // Act
+            alarme.Check();
+
+            // Assert
+            alarme.AlarmOn.Should().Be(true);
+        }
+    }
+}
