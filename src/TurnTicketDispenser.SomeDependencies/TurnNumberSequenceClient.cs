a0c3a79e430c192f90b813ce1821e13236d6e476
Rahul Sukhija <rahul.sukhija@nagarro.com>
2023-06-18 17:08:46 +0530

    refactor: add singleton pattern to TurnNumberSequence

--- src/TurnTicketDispenser.SomeDependencies/TurnNumberSequenceClient.cs
+++ src/TurnTicketDispenser.SomeDependencies/TurnNumberSequenceClient.cs
@@ -1,17 +1,18 @@
-﻿using System;
+﻿﻿using System;
 namespace TDDMicroExercises.TurnTicketDispenser.SomeDependencies
 {
     public class TurnNumberSequenceClient
     {
-		// A class with the only goal of simulating a dependency on TurnNumberSequence
-		// that has impact on the refactoring.
+        // A class with the only goal of simulating a dependency on TurnNumberSequence
+        // that has impact on the refactoring.
 
-		public TurnNumberSequenceClient()
+        public TurnNumberSequenceClient()
         {
             int nextUniqueTicketNumber;
-            nextUniqueTicketNumber = TurnNumberSequence.GetNextTurnNumber();
-			nextUniqueTicketNumber = TurnNumberSequence.GetNextTurnNumber();
-			nextUniqueTicketNumber = TurnNumberSequence.GetNextTurnNumber();
-		}
+            var turnNumberSequence = TurnNumberSequence.Instance;
+            nextUniqueTicketNumber = turnNumberSequence.GetNextTurnNumber();
+            nextUniqueTicketNumber = turnNumberSequence.GetNextTurnNumber();
+            nextUniqueTicketNumber = turnNumberSequence.GetNextTurnNumber();
+        }
     }
 }
--- src/TurnTicketDispenser/TicketDispenser.cs
+++ src/TurnTicketDispenser/TicketDispenser.cs
@@ -1,10 +1,19 @@
+using TDDMicroExercises.TurnTicketDispenser.Interfaces;
+
 namespace TDDMicroExercises.TurnTicketDispenser
 {
-    public class TicketDispenser
+    public class TicketDispenser : ITicketDispenser
     {
+        private readonly ITurnNumberSequence _turnNumberSequence;
+
+        public TicketDispenser(ITurnNumberSequence turnNumberSequence)
+        {
+            _turnNumberSequence = turnNumberSequence;
+        }
+
         public TurnTicket GetTurnTicket()
         {
-            int newTurnNumber = TurnNumberSequence.GetNextTurnNumber();
+            int newTurnNumber = _turnNumberSequence.GetNextTurnNumber();
             var newTurnTicket = new TurnTicket(newTurnNumber);
 
             return newTurnTicket;
